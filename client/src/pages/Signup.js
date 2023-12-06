20ed9be18c8472718e15ea8ef477e1238fea2d80
yannick <yannick.schlaepfer@lernende.bbw.ch>
2023-12-06 02:29:30 +0100

    changes to structure of signup

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -8,9 +8,9 @@ import axios from "axios";
 function Signup(){
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
-    const navigate = useNavigate();
     const [error, setError] = useState('');
     const [showErrorPopup, setShowErrorPopup] = useState(false);
+    const navigate = useNavigate();
 
     const createUser = async () => {
         try {
